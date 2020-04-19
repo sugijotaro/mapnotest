c9fc3627b57472b6fd250decb313fb52341902d7
sugijo <sugijo>
2020-04-19 15:52:26 +0900

    Initial Commit

---
+++ mapnotest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mapnotest
+//
+//  Created by JotaroSugiyama on 2020/04/19.
+//  Copyright © 2020 JotaroSugiyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
