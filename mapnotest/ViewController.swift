b20209d7fa88da795834b16fce60eb8c378d96b9
sugijo <sugijo>
2020-04-21 22:59:35 +0900

    今日の進捗

--- mapnotest/ViewController.swift
+++ mapnotest/ViewController.swift
@@ -9,15 +9,15 @@
 import UIKit
 import GoogleMaps
 
-class ViewController: UIViewController, CLLocationManagerDelegate, GMSMapViewDelegate {
+class ViewController: UIViewController, CLLocationManagerDelegate, GMSMapViewDelegate{
     
     var mapView: GMSMapView!
     var camera: GMSCameraPosition!
     private var locationManager: CLLocationManager?
     private var currentLocation: CLLocation?
-    private var zoomLevel: Float = 15.0
-    /// 初期描画の判断に利用
-//    private var initView: Bool = false
+    private var zoomLevel: Float = 17.8
+    
+//    @IBOutlet var tabBar: UITabBar!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,12 +36,30 @@ class ViewController: UIViewController, CLLocationManagerDelegate, GMSMapViewDel
         self.locationManager!.distanceFilter = 50
         self.locationManager!.startUpdatingLocation()
         self.locationManager!.delegate = self
+        
+        mapView.settings.compassButton = true
+        mapView.settings.myLocationButton = false
     }
+    
+//    func tabBar(tabBar: UITabBar, didSelectItem item: UITabBarItem){
+//        switch item.tag {
+//        case 1:
+//            print("1")
+//        case 2:
+//            print("2")
+//        default:
+//            return
+//        }
+//    }
 
     override func loadView() {
-        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 14.0)
+        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: self.zoomLevel, bearing: 0, viewingAngle: 45)
         mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-        mapView.isMyLocationEnabled = true
+        mapView.settings.scrollGestures = false
+        mapView.settings.zoomGestures = true
+        mapView.settings.tiltGestures = false
+        mapView.settings.rotateGestures = true
+        mapView.setMinZoom(15, maxZoom: 19)
         
         do {
             // Set the map style by passing the URL of the local file.
@@ -56,20 +74,9 @@ class ViewController: UIViewController, CLLocationManagerDelegate, GMSMapViewDel
         
         self.view = mapView
     }
-//
-//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-////      if !self.initView {
-//        // 初期描画時のマップ中心位置の移動
-//        camera = GMSCameraPosition.camera(withTarget: (locations.last?.coordinate)!, zoom: self.zoomLevel)
-//        self.mapView.camera = camera
-//        self.locationManager?.stopUpdatingLocation()
-////        self.initView = true
-////      }
-//        print("aaa")
-//    }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        camera = GMSCameraPosition.camera(withTarget: (locations.last?.coordinate)!, zoom: self.zoomLevel)
+        camera = GMSCameraPosition.camera(withTarget: (locations.last?.coordinate)!, zoom: self.zoomLevel, bearing: 0, viewingAngle: 45)
         self.mapView.camera = camera
         
         let location = locations.first
@@ -78,6 +85,14 @@ class ViewController: UIViewController, CLLocationManagerDelegate, GMSMapViewDel
 
         print("latitude: \(latitude!)\nlongitude: \(longitude!)")
     }
-
+    
+//    func didTapMyLocationButton(for mapView: GMSMapView) -> Bool {
+//
+//        guard let lat = mapView.myLocation?.coordinate.latitude,
+//            let lng = mapView.myLocation?.coordinate.longitude else { return false }
+//
+//        let camera = GMSCameraPosition.camera(withLatitude: lat ,longitude: lng , zoom: self.zoomLevel, bearing: 0, viewingAngle: 45)
+//        mapView.animate(to: camera)
+//        return true
+//    }
 }
-
